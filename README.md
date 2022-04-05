08d8bbbb66e396af748faedc3cfbacd552690f58
lullebulle2 <lvmd04@gmail.com>
2022-04-05 15:00:36 +0200

    påbörjat klass

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # skatteverket
+ Jag har börjat skriva mitt program. jag hade det lite svårt med att börja men med hjälp av Zion kunde jag börja göra min första class.
---
+++ skatteverket.py
@@ -0,0 +1,23 @@
+class Skatteverket:
+    def __init__(self, kommun, lön ) -> None:
+        self.kommun = kommun
+        self.lön = lön
+
+    def county_procent():
+        input("what county do you wark?")
+        #fortsätta med att skapa metod till att ta kommun och lön för att räkna ut.
+
+
+def main():
+    print("Hi and welcome to Skattevärket!! How can i help you?")
+    meny=input("""
+    get out your taxes baste on your county (the first one)
+    pay taxes (the secound one)
+    """)
+    if "the first one" in meny:
+        input("")
+
+
+
+__name__=="__main__"
+main()
