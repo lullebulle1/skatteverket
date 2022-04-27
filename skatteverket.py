03bbb77d69f1fbef561d0e455628e5eaa205f2d5
lullebulle2 <lvmd04@gmail.com>
2022-04-27 15:02:33 +0200

    förbättrat men meny lite inte helt klart

--- skatteverket.py
+++ skatteverket.py
@@ -1,4 +1,5 @@
-
+import os
+from unicodedata import name
 class County:
     def __init__(self, name, percentage) -> None:
         self.name = name
@@ -8,23 +9,40 @@ class County:
 class Citizen:
     def __init__(self, name) -> None:
         self.name = name
+        self.county = ""
 
     def municipality_procent(self, counties):
-        county = input("what municipality do you work at?")
-        for c in counties:
-            if c.name == county.capitalize():
-                break
-            self.procent_municipality(c)
-                
-                
-        else:
-            print("you need to put in what municipality you work at (in stockholms county ).")
+        while True:
+            county = input("in what municipality do you work at?")
+            for c in counties:
+                if c.name == county.lower():
+                    self.county = c
+                    self.procent_municipality()
+                    break
+                else:
+                    print("you need to put in what municipality you work at (in stockholms county ).")
 
-    def procent_municipality(self, county):
-        
+    def procent_municipality(self):
         salary = int(input("how much do you earn?"))
-        print(f"this is how much you need to pay:{salary*county.percentage}")
-
+        överskatt = 504000/12
+        åverskatt = 703000/12
+        if salary <= överskatt:
+            print(f"this is how much you need to pay:{salary*self.county.percentage}")
+                    
+        
+        elif salary > överskatt <= åverskatt:
+            överskatt1= salary - överskatt
+            överskatt2=överskatt1*0.5
+            print(f"this is how much you need to pay:{överskatt2*self.county.percentage + överskatt1}")
+            
+            #42000*0,3051= 12814,2
+            #3000*0,5=1500
+            #14314,2
+        elif salary > åverskatt:
+            åverskatt1 = salary - åverskatt
+            åverskatt2 = åverskatt1*0.55
+            print(f"this is how much you need to pay:{åverskatt2*self.county.percentage + åverskatt2}")
+            
         #fortsätta med att skapa metod till att ta kommun och lön för att räkna ut.
 
 def input_county():
@@ -39,17 +57,19 @@ def init_counties():
     with open("county.txt", "r", encoding="utf-8") as file:
         for line in file:
             atr = line.split(",")
-            counties.append(County(atr[0],atr[1]))
+            counties.append(County(atr[0],float(atr[1])))
     return counties
 
 def main():
-    print("Hi and welcome to Skattevärket!! How can i help you?")
-    counties = init_counties()
-    meny=input("""
-    get out your taxes baste on your county (the first one)
-    pay taxes (the secound one)
+    name=input("hi what is your name: ")
+    x = print(f"""Hi {name}, welcome to the tax authorities, how can we be at your service today? 
+    We can help you with ether geting out your taxes baste on your county or 
+    pay taxes 
     """)
-    if "the first one" in meny:
+    message = input("Here can you write what you would like to have help with: ")
+    counties = init_counties()
+    if "taxes" in message:
+        print(f"okay {name}that we can help you with. If you want in the end you can fill out a form to help us to bee bettre. Tanks.")
         nille = Citizen("Nille")
         nille.municipality_procent(counties)
 
