5829d04768bcbfcc6c9ad6aede35ef61f4bb54d0
lullebulle2 <lvmd04@gmail.com>
2022-05-09 15:55:09 +0200

    fixat så att save file systemt funkar. och utökat grenar i programmet så det går att göra mer saker.

--- skatteverket.py
+++ skatteverket.py
@@ -1,14 +1,10 @@
 import os
-from tkinter import N
-
-
 
 def clearconsole():
     command = 'clear'
     if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
-
 
 def init_counties():
     counties = []
@@ -18,43 +14,49 @@ def init_counties():
             counties.append(County(atr[0],float(atr[1])))
     return counties
 
-
 def meny():
-    yes=input(f"Do you need help with any thing els?")
-    if "y" in yes:
-        clearconsole()
-        print("""
-    [1]geting out your taxes baste on your county 
-    [2]pay taxes
-    [3]if you dont need anything, write '#' """)
-    else:
-        print(f"Very well, see you next time {name}")
-        quit()
-    message = input("Here can you write what you would like to have help with: ")
-    counties = init_counties()
-    if "1" in message:
-        print(f"okay that we can help you with. If you want in the end you can fill out a form to help us to bee bettre. Tanks.")
-        nille = Citizen("Nille")
-        nille.municipality_procent(counties)
-    elif "2" in message:
-        pass
-    elif "#" in message:
-        form = input("do you want to fill out a form to custemer survice? (y/n)")
-        if "y" in form:
-            forms()
-            quit()
+    with open("persanol_information.txt", "a", encoding="utf-8") as p:
+        yes=input(f"Do you need help with any thing els? (y/n)")
+        if "y" in yes:
+            clearconsole()
+            print("""
+        [1]geting out your taxes baste on your county 
+        [2]pay taxes
+        [3]if you dont need anything, write '#' """)
         else:
             print(f"Very well, see you next time {name}")
             quit()
+        messager = input("Here can you write what you would like to have help with: ")
+        counties = init_counties()
+        if "1" in messager:
+            print(f"okay that we can help you with. If you want in the end you can fill out a form to help us to bee bettre. Tanks.")
+            nille = Citizen("Nille")
+            nille.municipality_procent(counties)
+        elif "2" in messager:
+            pass
+        elif "#" in messager:
+            form = input("do you want to fill out a form to custemer survice? (y/n)")
+            if "y" in form:
+                forms()
+                quit()
+            else:
+                print(f"Very well, see you next time {name}")
+                quit()
+        elif "no" in yes:
+            form = input("do you want to fill out a form to custemer survice? (y/n)")
+            if "y" in form:
+                forms()
+                quit()
 
 def forms():
     with open("service.txt", "a", encoding="utf-8") as s:
         print("press ENTER to move on")
-        s.write(input("How was your experiens? "))
-        s.write(input("Did you have any problems? "))
-        s.write(input("What was your problem? "))
-        s.write(input("How can we do bettre?"))
+        s.write(input("How was your experiens? ") + "\n")
+        s.write(input("Did you have any problems? ") + "\n")
+        s.write(input("What was your problem? ") + "\n")
+        s.write(input("How can we do bettre?") + "\n")
         print("Thank you for your suport we will take it to consideration.")
+
         
 
 class County:
@@ -87,8 +89,8 @@ class Citizen:
       #      print(elev, end="")
             
 
-    def municipality_procent(self, counties):
-        with open("persanol_information.txt", "a", encoding="utf-8") as p:
+    def municipality_procent_extra(self, counties):
+        with open("persanol_information_pay_taxes.txt", "a", encoding="utf-8") as p:
             while True:
                 county = input("in what municipality do you work at?")
                 for c in counties:
@@ -96,28 +98,35 @@ class Citizen:
                         self.county = c
                         p.write(county)
                         p.close()
-                        self.procent_municipality()
+                        self.procent_municipality_extra()
                         
                         break
                     else:
                         print("you need to put in what municipality you work at (in stockholms county ).")
-                
-
-    def procent_municipality(self):
-        with open("persanol_information.txt", "a", encoding="utf-8") as p:
+    def pay_taxes(self):
+        print(f"you need to pay {self.procent_municipality}")          
+    
+    def procent_municipality_extra(self):
+        with open("persanol_information_pay_taxes.txt", "a", encoding="utf-8") as p:
             salary = int(input("how much do you earn?"))
             överskatt = 504000/12 #42 000kr
             åverskatt = 703000/12 #58 583kr
             if salary <= överskatt:
-                p.write(print(f"this is how much you need to pay:{salary*self.county.percentage}"))
-                meny()
-                p.write(salary)
+                a = salary*self.county.percentage
+                total_salary= (f"original salary = {salary} you will pay {a} as taxes\n \n")
+                print(total_salary)
+                p.write(total_salary)
                 p.close()
-            
+                self.pay_taxes()
+                
             elif salary > överskatt and salary <= åverskatt:
                 överskatt1= salary - överskatt
                 överskatt2=överskatt1*0.5
-                p.write.str((print(f"this is how much you need to pay:{överskatt*self.county.percentage + överskatt2}")))
+                b = överskatt*self.county.percentage + överskatt2
+                total_salary = (f"original salary = {salary} you will pay {b} as taxes\n \n")
+                print(total_salary)
+                p.write(total_salary)
+                p.close()
                 meny()
                 #42000*0,3051= 12814,2
                 #3000*0,5=1500
@@ -125,28 +134,91 @@ class Citizen:
             elif salary > åverskatt:
                 åverskatt1 = salary - åverskatt
                 åverskatt2 = åverskatt1*0.55
-                p.write(print(f"this is how much you need to pay:{åverskatt*self.county.percentage + åverskatt2}"))
+                c = åverskatt*self.county.percentage + åverskatt2
+                total_salary = (f"original salary = {salary} you will pay {c} as taxes\n \n")
+                print(total_salary)
+                p.write(total_salary)
+                p.close()
+                meny()
+            #fortsätta med att skapa metod till att ta kommun och lön för att räkna ut.
+
+
+
+    def municipality_procent(self, counties):
+        with open("persanol_information.txt", "a", encoding="utf-8") as p:
+            while True:
+                county = input("in what municipality do you work at?")
+                for c in counties:
+                    if c.name == county.lower():
+                        self.county = c
+                        p.write("Where do you work at: " + county + "\n")
+                        p.close()
+                        self.procent_municipality()
+                        
+                        break
+                    else:
+                        print("you need to put in what municipality you work at (in stockholms county ).")
+
+    def procent_municipality(self):
+        with open("persanol_information.txt", "a", encoding="utf-8") as p:
+            salary = int(input("how much do you earn?"))
+            överskatt = 504000/12 #42 000kr
+            åverskatt = 703000/12 #58 583kr
+            if salary <= överskatt:
+                a = salary*self.county.percentage
+                total_salary= (f"original salary = {salary} you will pay {a} as taxes\n \n")
+                print(total_salary)
+                p.write(total_salary)
+                p.close()
+                meny()
+                           
+            elif salary > överskatt and salary <= åverskatt:
+                överskatt1= salary - överskatt
+                överskatt2=överskatt1*0.5
+                b = överskatt*self.county.percentage + överskatt2
+                total_salary = (f"original salary = {salary} you will pay {b} as taxes\n \n")
+                print(total_salary)
+                p.write(total_salary)
+                p.close()
+                meny()
+                #42000*0,3051= 12814,2
+                #3000*0,5=1500
+                #14314,2
+            elif salary > åverskatt:
+                åverskatt1 = salary - åverskatt
+                åverskatt2 = åverskatt1*0.55
+                c = åverskatt*self.county.percentage + åverskatt2
+                total_salary = (f"original salary = {salary} you will pay {c} as taxes\n \n")
+                print(total_salary)
+                p.write(total_salary)
+                p.close()
                 meny()
             #fortsätta med att skapa metod till att ta kommun och lön för att räkna ut.
 
 def main():
-    global name
+    clearconsole()
     with open("persanol_information.txt", "a", encoding="utf-8") as p:
-        name=input("Hi what is your name: ")
+        name = input("Hi what is your name: ")
         print(f"Hi {name}, welcome to the tax authorities, how can we be at your service today?") 
         print(f"""
         [1]geting out your taxes baste on were you work
         [2]pay taxes""")
+
+        p.write("formuler: \n"+ name + "\n")
+
         message = input("Here can you write what you would like to have help with: ")
         counties = init_counties()
+
         if "1" in message:
             print(f"okay that we can help you with. If you want in the end you can fill out a form to help us to bee bettre. Tanks.")
+            p.write("geting out your taxes baste on were you work:" + message + "\n")
+            p.close()
             nille = Citizen("Nille")
             nille.municipality_procent(counties)
+
         elif "2" in message:
-            pass
-    p.write(name, message)
-
-
+            nille = Citizen("Nille")
+            nille.municipality_procent_extra(counties)
+            
 if __name__=="__main__":
     main()
